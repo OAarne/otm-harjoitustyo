2feec2b9a37603c50158e6a45d7b0b4cc96728ee
OAarne <onni.aarne@gmail.com>
2018-03-22 15:11:23 +0200

    make it show a window (!!)

--- zhuLi/src/main/kotlin/zhuLi/views/MainView.kt
+++ zhuLi/src/main/kotlin/zhuLi/views/MainView.kt
@@ -1,15 +1,30 @@
 package zhuLi.views
 
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.layout.VBox
 import javafx.scene.web.WebView
 import tornadofx.View
 
-class MainView : View() {
+//class MainView : View() {
+//
+////    override val root = WebView()
+//    override val root = VBox()
+//
+//    init {
+//        root += Button("Press Me")
+//        root += Label("Waiting")
+////        with(root) {
+////            setPrefSize(800.0, 600.0)
+////        }
+//    }
+//}
 
-    override val root = WebView()
+class MainView: View() {
+    override val root = VBox()
 
     init {
-        with(root) {
-            setPrefSize(800.0, 600.0)
-        }
+        root.children.add(Label("Waiting"))
+        root.children.add(Button("Press Me"))
     }
 }
