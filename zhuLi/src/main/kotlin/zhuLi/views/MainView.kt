843d668286c301a478414cbb01d2a5ee2f351d00
OAarne <onni.aarne@gmail.com>
2018-04-01 20:24:54 +0300

    Laskarit viikko 3 DONE

--- zhuLi/src/main/kotlin/zhuLi/views/MainView.kt
+++ zhuLi/src/main/kotlin/zhuLi/views/MainView.kt
@@ -6,25 +6,26 @@ import javafx.scene.layout.VBox
 import javafx.scene.web.WebView
 import tornadofx.View
 
-//class MainView : View() {
-//
-////    override val root = WebView()
+class MainView : View() {
+
+    override val root = WebView()
+//    override val root = VBox()
+
+    init {
+//        root.children.add(Label("Waiting"))
+//        root.children.add(Button("Press Me"))
+        root.setPrefSize(800.0, 600.0);
+//        with(root) {
+//            setPrefSize(800.0, 600.0)
+//        }
+    }
+}
+
+//class MainView: View() {
 //    override val root = VBox()
 //
 //    init {
-//        root += Button("Press Me")
-//        root += Label("Waiting")
-////        with(root) {
-////            setPrefSize(800.0, 600.0)
-////        }
+//        root.children.add(Label("Waiting"))
+//        root.children.add(Button("Press Me"))
 //    }
 //}
-
-class MainView: View() {
-    override val root = VBox()
-
-    init {
-        root.children.add(Label("Waiting"))
-        root.children.add(Button("Press Me"))
-    }
-}
